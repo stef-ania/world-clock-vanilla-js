4bda3ac96ebc1eceafa20528858a67915de8964a
Stefania <stebofof@gmail.com>
2023-09-29 10:14:01 +0200

    Refactored JS script

--- src/script.js
+++ src/script.js
@@ -17,57 +17,87 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
-let cityTimeZone = null; // Initialize the selected cityTimeZone as null
+let cityTimeZone = null;
 
-function showCitySelected(event) {
-  let newCityTimeZone = event.target.value;
+// Function to update the city information
+function updateCityInfo(cityTimeZone) {
+  const cityTime = moment().tz(cityTimeZone);
+  const cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  const cityDateFormat = cityTime.format("MMMM Do YYYY");
+  const cityTimeFormat = cityTime.format("hh:mm:ss");
+  const cityTimeFormatA = cityTime.format("A");
 
-  // Check if a valid city option is selected
-  if (newCityTimeZone !== "Select a city...") {
-    if (newCityTimeZone === "current") {
-      newCityTimeZone = moment.tz.guess();
-    }
-
-    // Only update the selected city if it has changed
-    if (newCityTimeZone !== cityTimeZone) {
-      cityTimeZone = newCityTimeZone;
-
-      let citiesElement = document.querySelector("#cities");
-
-      // Function to update the time
-      function updateCityTime() {
-        let cityTime = moment().tz(cityTimeZone);
-        let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-        let cityDateFormat = cityTime.format("MMMM Do YYYY");
-        let cityTimeFormat = cityTime.format("hh:mm:ss");
-        let cityTimeFormatA = cityTime.format("A");
-
-        // Create the HTML for the selected city
-        let cityHTML = `
-          <article class="city">
-            <div>
-              <h2>${cityName}</h2>
-              <div class="date">${cityDateFormat}</div>
-            </div>
-            <div class="time">${cityTimeFormat} <small>${cityTimeFormatA}</small></div>
-          </article>
-        `;
-
-        // Update the content of the citiesElement without replacing it
-        citiesElement.innerHTML = cityHTML;
-      }
-
-      // Call the update function initially and then set up an interval
-      updateCityTime();
-      setInterval(updateCityTime, 1000);
-    }
-  } else {
-    // Handle the case when "Select a city..." is chosen
-    // You can clear the city information or display a message as needed
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = "Please select a city...";
-  }
+  return {
+    cityName,
+    cityDateFormat,
+    cityTimeFormat,
+    cityTimeFormatA,
+  };
 }
 
-let selectCity = document.querySelector("#select-city");
-selectCity.addEventListener("change", showCitySelected);
+// Function to update the HTML content with city information
+function updateHTML(cityInfo) {
+  const citiesElement = document.querySelector("#cities");
+  const cityHTML = `
+    <div class="svg-container">
+        <img src="" alt="" id="SVG-img" />
+    </div>
+    <article class="city">
+      <div>
+        <h2>${cityInfo.cityName}</h2>
+        <div class="date">${cityInfo.cityDateFormat}</div>
+      </div>
+      <div class="time">${cityInfo.cityTimeFormat} <small>${cityInfo.cityTimeFormatA}</small></div>
+    </article>
+  `;
+  citiesElement.innerHTML = cityHTML;
+}
+
+// Function to handle city selection
+function handleCitySelection(event) {
+  const newCityTimeZone = event.target.value;
+
+  if (newCityTimeZone === "Select a city...") {
+    // Handle the case when "Select a city..." is chosen
+    const citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = "Please select a city...";
+    return;
+  }
+
+  if (newCityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  } else {
+    cityTimeZone = newCityTimeZone;
+  }
+
+  // Update the city information and HTML content
+  const cityInfo = updateCityInfo(cityTimeZone);
+  updateHTML(cityInfo);
+
+  // Set up an interval to update the time every second
+  setInterval(() => {
+    const cityInfo = updateCityInfo(cityTimeZone);
+    updateHTML(cityInfo);
+  }, 1000);
+}
+
+// Add event listener for city selection
+const selectCity = document.querySelector("#select-city");
+selectCity.addEventListener("change", handleCitySelection);
+
+function updateCitySVG() {
+  //let currentTimeSVG = document.querySelector("#current-time-SVG");
+  //currentTimeSVG.setAttribute("src", `img/${}.svg`);
+
+  const time = document.querySelector(".time");
+
+  if (time > "00:00:00 <small>AM</small>" && time < "11:59:59 <small>AM</small>") {
+    document.getElementById("SVG-img").src = "img/morning.svg";
+  } else {
+    document.getElementById("SVG-img").src = "img/night.svg";
+  }
+
+  // Display the SVG content
+  let svgContainer = document.querySelector("#svg-container");
+  svgContainer.style.display = "block";
+}
