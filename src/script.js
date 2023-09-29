890f4c207fe658866965da0339e8656f76193268
Stefania <stebofof@gmail.com>
2023-09-29 11:33:41 +0200

    Added an image that change depending on the time zone selected

--- src/script.js
+++ src/script.js
@@ -19,6 +19,14 @@ setInterval(updateTime, 1000);
 
 let cityTimeZone = null;
 
+// Create an object with the path of the SVG images
+const svgImages = {
+  sunrise: "img/sunrise.svg",
+  morning: "img/morning.svg",
+  sunset: "img/sunset.svg",
+  night: "img/night.svg",
+};
+
 // Function to update the city information
 function updateCityInfo(cityTimeZone) {
   const cityTime = moment().tz(cityTimeZone);
@@ -27,20 +35,42 @@ function updateCityInfo(cityTimeZone) {
   const cityTimeFormat = cityTime.format("hh:mm:ss");
   const cityTimeFormatA = cityTime.format("A");
 
+  // Determine the time period based on the AM/PM format and hour
+  const isAM = cityTimeFormatA.toLowerCase() === "am";
+  const hours = cityTime.hours();
+
+  const timePeriod =
+    isAM && hours >= 0 && hours < 5
+      ? "night"
+      : isAM && hours >= 5 && hours < 7
+      ? "sunrise"
+      : isAM && hours >= 7 && hours < 12
+      ? "morning"
+      : !isAM && hours >= 12 && hours < 17
+      ? "morning"
+      : !isAM && hours >= 17 && hours < 21
+      ? "sunset"
+      : !isAM && hours >= 21 && hours < 24
+      ? "night"
+      : "night"; // Default to "night" for any other cases
+
   return {
     cityName,
     cityDateFormat,
     cityTimeFormat,
     cityTimeFormatA,
+    timePeriod, // Add the time period to the returned object
   };
 }
 
 // Function to update the HTML content with city information
 function updateHTML(cityInfo) {
   const citiesElement = document.querySelector("#cities");
+  const svgContainer = document.querySelector(".svg-container > img");
+
   const cityHTML = `
     <div class="svg-container">
-        <img src="" alt="" id="SVG-img" />
+        <img src="${svgImages[cityInfo.timePeriod]}" alt="${cityInfo.timePeriod}" class="SVG-time-of-day" />
     </div>
     <article class="city">
       <div>
@@ -49,7 +79,7 @@ function updateHTML(cityInfo) {
       </div>
       <div class="time">${cityInfo.cityTimeFormat} <small>${cityInfo.cityTimeFormatA}</small></div>
     </article>
-  `;
+ `;
   citiesElement.innerHTML = cityHTML;
 }
 
@@ -84,20 +114,3 @@ function handleCitySelection(event) {
 // Add event listener for city selection
 const selectCity = document.querySelector("#select-city");
 selectCity.addEventListener("change", handleCitySelection);
-
-function updateCitySVG() {
-  //let currentTimeSVG = document.querySelector("#current-time-SVG");
-  //currentTimeSVG.setAttribute("src", `img/${}.svg`);
-
-  const time = document.querySelector(".time");
-
-  if (time > "00:00:00 <small>AM</small>" && time < "11:59:59 <small>AM</small>") {
-    document.getElementById("SVG-img").src = "img/morning.svg";
-  } else {
-    document.getElementById("SVG-img").src = "img/night.svg";
-  }
-
-  // Display the SVG content
-  let svgContainer = document.querySelector("#svg-container");
-  svgContainer.style.display = "block";
-}
